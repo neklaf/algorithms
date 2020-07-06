af503b10491d928b2f06b3b5520af2c046acf40d
neklaf <aitor.acedo@gmail.com>
2020-07-06 23:24:26 +0200

    Added a problem to solve

---
+++ common/find_closest_target.py
@@ -0,0 +1,9 @@
+'''
+The problem which is being solved here is that takes two arrays of numbers, 
+let's assume that the two arrays have the same length, and it's needed
+to implement an algorithm that returns 2 numbers from these arrays with the
+closest sum to a third element called target.
+
+Reference: https://www.youtube.com/watch?v=GBuHSRDGZBY
+https://www.csdojo.io/problem
+'''
