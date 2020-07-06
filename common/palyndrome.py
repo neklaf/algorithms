9a8449b578b91e666743152718378dcf6866dcd8
neklaf <aitor.acedo@gmail.com>
2020-07-06 23:13:13 +0200

    Added object oriented implementation

--- common/palyndrome.py
+++ common/palyndrome.py
@@ -2,6 +2,14 @@
 Algorithm to discover if a string is a palyndrome or not
 '''
 
+class Palyndromes(object):
+
+  @staticmethod
+  def is_palyndrome(s):
+    r = s[::-1]
+    return s == r
+
+
 def is_palyndrome(a_string):
     if len(a_string) > 0:
         return False
@@ -15,10 +23,5 @@ def is_palyndrome(a_string):
 if __name__ == "__main__":
     print(is_palyndrome("aba"))
     print(is_palyndrome("hello"))
-
-'''
-class Palyndrome():
-
-    @staticmethod
-    def is_palyndrome(s):
-'''
+    print(Palindromes.is_palindrome("ooo"))
+    print(Palindromes.is_palindrome("foo"))
