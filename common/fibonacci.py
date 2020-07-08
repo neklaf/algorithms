e860f612ba67aef6e533b9794d5da60781f40bc1
neklaf <aitor.acedo@gmail.com>
2020-07-08 18:17:02 +0200

    Added new implementations to Fibonacci sequence

--- common/fibonacci.py
+++ common/fibonacci.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 '''
 Fibonacci sequence is defined as follows: the first number of the sequence is 0,
 the second number of the sequence is 1, and the nth number is the sum of the 
@@ -26,11 +28,60 @@ def getNthFib(n):
     else:
         return (getNthFib(n-1) + getNthFib(n-2))
 
+'''
+Dynamic Programming using memoization
+'''
+fibonacci_cache = {}
+
+def fibonacci_memo(n):
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+    
+    if n == 0:
+        value = 0
+    elif n == 1:
+        value = 1
+    else:
+        value = (fibonacci_memo(n-1) + fibonacci_memo(n-2))
+
+    fibonacci_cache[n] = value
+    return value
+
+
+'''
+Implement this problem using Python tools as LRU (Least Recently Used) 
+cache, which provides a decorator
+'''
+
+
+@lru_cache(maxsize = 1000)
+def fibonacci_lru(n):
+    if type(n) != int:
+        raise TypeError("n must be an integer number")
+    #if n < 1:
+    #    raise ValueError("n has to be positive")
+
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_lru(n-1) + fibonacci_lru(n-2)
+
 if __name__ == "__main__":
-    fibonacci()
+    #fibonacci()
+    '''
+    for i in range(0, 20):
+        print(getNthFib(i))
 
-    print(getNthFib(7))
+    for n in range(0, 50):
+        print("Fib(",n,")",fibonacci_memo(n))
+    '''
+    print("Using LRU")
+    for i in range(1, 51):
+        print("Fib(",i,")",fibonacci_lru(i))
 
-'''
-Dynamic Programming solution
-'''
+    print("Golden ratio")
+    for i in range(1, 51):
+        print("Golden(",i,")",fibonacci_lru(i+1) / fibonacci_lru(i))
+    
